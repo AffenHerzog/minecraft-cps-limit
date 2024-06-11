a902b3e91fe22d87246bb47ac8452f60b7af1a89
Julius <julius.rothgaenger@gmail.com>
2024-06-11 21:59:06 +0200

    add ProtocolLib and Lombok dependency, register the protocolManager

--- src/main/java/de/affenherzog/cpslimit/CpsLimit.java
+++ src/main/java/de/affenherzog/cpslimit/CpsLimit.java
@@ -1,17 +1,26 @@
 package de.affenherzog.cpslimit;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CpsLimit extends JavaPlugin {
 
+    @Getter
+    private static CpsLimit instance;
+
+    @Getter
+    private ProtocolManager protocolManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        this.protocolManager = ProtocolLibrary.getProtocolManager();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
