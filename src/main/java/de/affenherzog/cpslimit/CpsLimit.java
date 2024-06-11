2ee642e396572c1e1414ddb4f6c46089d6baa81e
Julius <julius.rothgaenger@gmail.com>
2024-06-11 21:43:00 +0200

    init project

---
+++ src/main/java/de/affenherzog/cpslimit/CpsLimit.java
@@ -0,0 +1,17 @@
+package de.affenherzog.cpslimit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CpsLimit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
